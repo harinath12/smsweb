0de46a2fb47f8941a1c37f7e4e1ee66b5c8f873e
harinath12 <harinathit@gmail.com>
2019-10-24 16:27:54 +0530

    api calendar

--- api/calendar.php
+++ api/calendar.php
@@ -10,20 +10,24 @@ function get_calendar(){
     {
         
         while($row=@mysql_fetch_assoc($exe)){
-            if($row['calendar_type'] == 1){
-                $tdata = array('title' => $row['calendar_title'], 'start' => $row['calendar_start_date'], 'className' => array('event', 'greenEvent'));
-            } elseif($row['calendar_type'] == 2) {
-                $tdata = array('title' => $row['calendar_title'], 'start' => $row['calendar_start_date'], 'className' => array('event', 'blueEvent'));
-            }
-            if($row['calendar_end_date']!="") { 
-                while(strtotime($row['calendar_start_date'])<=strtotime($row['calendar_end_date'])){
-                   $tdata['start'] = $row['calendar_start_date'];
-                   $data[] = $tdata; 
-                   $row['calendar_start_date'] = date('Y-m-d', strtotime('+1 day', strtotime($row['calendar_start_date'])));
+            $classes = explode(',',$row['calendar_classes']);
+            if(count($classes) == 0 || in_array('All', $classes) || in_array($_GET['class'], $classes)){
+                if($row['calendar_type'] == 1){
+                    $tdata = array('title' => $row['calendar_title'], 'start' => $row['calendar_start_date'], 'className' => array('event', 'greenEvent'));
+                } elseif($row['calendar_type'] == 2) {
+                    $tdata = array('title' => $row['calendar_title'], 'start' => $row['calendar_start_date'], 'className' => array('event', 'blueEvent'));
                 }
-            } else {
-                $data[] = $tdata;
+                if($row['calendar_end_date']!="") { 
+                    while(strtotime($row['calendar_start_date'])<=strtotime($row['calendar_end_date'])){
+                       $tdata['start'] = $row['calendar_start_date'];
+                       $data[] = $tdata; 
+                       $row['calendar_start_date'] = date('Y-m-d', strtotime('+1 day', strtotime($row['calendar_start_date'])));
+                    }
+                } else {
+                    $data[] = $tdata;
+                }
             }
+            
             
         }
     }
